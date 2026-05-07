127fc71c9a05e8617284963b86cdfa5dd8203804
Fernando Magalhaes <fernandomagalhaes@users.noreply.github.com>
2026-05-07 12:38:29 -0500

    Connect Firebase project config

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 window.MEDDPICC_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: ""
+  apiKey: "AIzaSyBFBVzlJsLwlm2EjfWLI9RMs3nkBgsgLBs",
+  authDomain: "ornate-acronym-461420-b6.firebaseapp.com",
+  projectId: "ornate-acronym-461420-b6",
+  storageBucket: "ornate-acronym-461420-b6.firebasestorage.app",
+  messagingSenderId: "1073314830807",
+  appId: "1:1073314830807:web:56b308f4e59a876a96d392"
 };
-
